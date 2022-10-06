d1d4e332af79968d0f726f23769c24c052011005
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-06 22:14:49 +0900

    ントローラやビューでアソシエーションを使う

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,4 +9,8 @@ class BlogsController < ApplicationController
     @blog = current_user.blogs.build(blog_params)
     render :new if @blog.invalid?
   end
+
+  def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
+  end
 end
