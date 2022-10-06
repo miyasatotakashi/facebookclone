077df475ca5a1ebfd58805774e3407f0a9d5f91b
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-06 23:15:24 +0900

    letter_opener_webをインストールし実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :favorites, only: [:create, :destroy]
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  
 end
