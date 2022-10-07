aa9404b27ff70a02ca180abc4d2904ed715ca981
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-07 11:20:09 +0900

    画像をアップロードする仕組みを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_06_134637) do
+ActiveRecord::Schema.define(version: 2022_10_07_021302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2022_10_06_134637) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["blog_id"], name: "index_favorites_on_blog_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
