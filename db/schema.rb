41f1bf9f0182ab04e636522e8d3046dd7e3a177a
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-09 01:49:34 +0900

    feedとuserを紐付けた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_125831) do
+ActiveRecord::Schema.define(version: 2022_10_08_163322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_125831) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "comment"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -67,4 +69,5 @@ ActiveRecord::Schema.define(version: 2022_10_07_125831) do
   add_foreign_key "blogs", "users"
   add_foreign_key "favorites", "blogs"
   add_foreign_key "favorites", "users"
+  add_foreign_key "feeds", "users"
 end
